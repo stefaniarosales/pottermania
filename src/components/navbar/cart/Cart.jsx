323c79dec169b300740548ea2f2fc6822ae0eeef
stefaniarosales <stefaniarosales@live.com>
2024-10-23 12:07:35 -0300

    add: modal "carrito vacio"

--- src/components/navbar/cart/Cart.jsx
+++ src/components/navbar/cart/Cart.jsx
@@ -1,8 +1,8 @@
-import React, { useMemo, useRef, useEffect, useState} from 'react';
+import React, { useMemo, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity, clearCart, toggleCart} from '../../../redux/cartSlice';
+import { removeItem, updateQuantity, clearCart, toggleCart } from '../../../redux/cartSlice';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 //styles
 import {
   StyledContainer,
@@ -18,20 +18,50 @@ import {
   StyledActions,
   StyledActionButton,
   StyledTotal,
-} from './styledcart'
+} from './styledcart';
 
 const Cart = () => {
-  const items = useSelector((state) => state.cart.items)
-  const isOpen = useSelector((state) => state.cart.isOpen)
-  const dispatch = useDispatch()
+  const items = useSelector((state) => state.cart.items);
+  const isOpen = useSelector((state) => state.cart.isOpen);
+  const dispatch = useDispatch();
 
-  const [isClickOutside, setIsClickOutside] = useState(false);
-  const cartRef = useRef(null)
+  const cartRef = useRef(null);
 
   const handleRemoveItem = (id) => {
-    dispatch(removeItem({ id }))
+    dispatch(removeItem({ id }));
 
-    // Configuración del Toast para eliminar un producto
+    // Toast para eliminar un producto
+    Swal.fire({
+      icon: 'error',
+      title: '¡Producto eliminado!',
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+    });
+  };
+
+  const handleQuantityChange = (id, delta) => {
+    setQuantity(id, delta);
+  };
+
+  const setQuantity = (id, delta) => {
+    const item = items.find(item => item.id === id);
+    if (item) {
+      const newQuantity = item.quantity + delta;
+      if (newQuantity > 0) {
+        dispatch(updateQuantity({ id, quantity: newQuantity }));
+      }
+    }
+  };
+
+  // Borrar todos los productos del carrito
+  const handleClearCart = () => {
+    dispatch(clearCart());
+    dispatch(toggleCart());
+
+    // Configuración del Toast para carrito vacío
     const Toast = Swal.mixin({
       toast: true,
       position: 'top-end',
@@ -45,28 +75,9 @@ const Cart = () => {
     });
     // Mostrar el Toast
     Toast.fire({
-      icon: 'error',
-      title: '¡Producto eliminado!',
+      icon: 'info',
+      title: 'Carrito vacío',
     });
-  }
-
-  const handleQuantityChange = (id, delta) => {
-    setQuantity(id, delta);
-  }
-
-  const setQuantity = (id, delta) => {
-    const item = items.find(item => item.id === id);
-    if (item) {
-      const newQuantity = item.quantity + delta;
-      if (newQuantity > 0) {
-        dispatch(updateQuantity({ id, quantity: newQuantity }));
-      }
-    }
-  }
-
-  //borrar todos los productos del carrito
-  const handleClearCart = () => {
-    dispatch(clearCart());
   };
 
   // Calcular el total del carrito
@@ -77,16 +88,17 @@ const Cart = () => {
   // Mostrar modal de éxito al finalizar compra
   const handleFinishPurchase = () => {
     dispatch(clearCart());
+    dispatch(toggleCart());
 
     // SweetAlert para mostrar el modal de éxito
     Swal.fire({
       title: '¡Compra realizada con éxito!',
       text: 'Gracias por tu compra',
       icon: 'success',
-      timer: 3000, 
+      timer: 2000,
       showConfirmButton: false,
-    })
-  }
+    });
+  };
 
   // Cerrar el carrito si se hace clic fuera del componente
   useEffect(() => {
@@ -95,59 +107,50 @@ const Cart = () => {
         dispatch(toggleCart()); // Cierra el carrito si está abierto
       }
     };
-  
+
     document.addEventListener('mousedown', handleClickOutside);
-  
+
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
-    }
+    };
   }, [dispatch]);
-  useEffect(() => {
-    if (isClickOutside && isOpen) {
-      dispatch(toggleCart()); // Cierra el carrito
-      setIsClickOutside(false); // Resetea el estado para futuras detecciones de clic fuera
-    }
-  }, [isClickOutside, isOpen, dispatch])
 
   return (
-  isOpen && (
-    <StyledContainer ref={cartRef}>
-      {items.length === 0 ? (<p>Carrito vacio</p>) 
-      :(
-        <>
-          <StyledSubtitleCart>Tus Compras </StyledSubtitleCart>
-          <StyledItemsList>
-            {items.map(item => (
-              <StyledItem key={item.id}>
-                <StyledItemDetails>
-                  <StyledItemTitle>{item.title}</StyledItemTitle>
-                  <StyledItemPrice>${item.price}</StyledItemPrice>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <StyledQuantityButton onClick={() => handleQuantityChange(item.id,-1)}>-</StyledQuantityButton>
-                    <StyledItemQuantity
-                      type="number"
-                      value={item.quantity}
-                      readOnly
-                    />
-                    <StyledQuantityButton onClick={() => handleQuantityChange(item.id, 1)}>+</StyledQuantityButton>
-                  </div>
-                </StyledItemDetails>
-                <StyledItemRemoveButton onClick={() => handleRemoveItem(item.id)}>
-                  <DeleteIcon />
-                </StyledItemRemoveButton>
-              </StyledItem>
-            ))}
-          </StyledItemsList>
-          <StyledTotal>Total: ${totalPrice.toFixed(2)}</StyledTotal>
-          <StyledActions>
-            <StyledActionButton onClick={handleClearCart}>Borrar productos</StyledActionButton>
-            <StyledActionButton onClick={handleFinishPurchase}>Finalizar compra</StyledActionButton>
-          </StyledActions>
-        </>
-      )}
-    </StyledContainer>
-  )
-  )
-}
+    isOpen && (
+      <StyledContainer ref={cartRef}>
+        {items.length === 0 ? (
+          <p>Carrito vacío</p>
+        ) : (
+          <>
+            <StyledSubtitleCart>Tus Compras</StyledSubtitleCart>
+            <StyledItemsList>
+              {items.map(item => (
+                <StyledItem key={item.id}>
+                  <StyledItemDetails>
+                    <StyledItemTitle>{item.title}</StyledItemTitle>
+                    <StyledItemPrice>${item.price}</StyledItemPrice>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <StyledQuantityButton onClick={() => handleQuantityChange(item.id, -1)}>-</StyledQuantityButton>
+                      <StyledItemQuantity type="number" value={item.quantity} readOnly />
+                      <StyledQuantityButton onClick={() => handleQuantityChange(item.id, 1)}>+</StyledQuantityButton>
+                    </div>
+                  </StyledItemDetails>
+                  <StyledItemRemoveButton onClick={() => handleRemoveItem(item.id)}>
+                    <DeleteIcon />
+                  </StyledItemRemoveButton>
+                </StyledItem>
+              ))}
+            </StyledItemsList>
+            <StyledTotal>Total: ${totalPrice.toFixed(2)}</StyledTotal>
+            <StyledActions>
+              <StyledActionButton onClick={handleClearCart}>Borrar productos</StyledActionButton>
+              <StyledActionButton onClick={handleFinishPurchase}>Finalizar compra</StyledActionButton>
+            </StyledActions>
+          </>
+        )}
+      </StyledContainer>
+    )
+  );
+};
 
-export default Cart
+export default Cart;
