08fe08834561d4bf557b4c06bd3c58619750ee5b
stefaniarosales <stefaniarosales@live.com>
2024-11-05 16:01:08 -0300

    add: no page

--- src/pages/nopage/NoPage.jsx
+++ src/pages/nopage/NoPage.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+// NoPage.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-function NoPage() {
+const NoPage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <Content>
+        <StyledLink to="/">Volver a la página principal</StyledLink>
+      </Content>
+    </Container>
+  );
+};
 
-export default NoPage
+export default NoPage;
+
+// Estilos con styled-components
+const Container = styled.div`
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  padding-bottom: 120px;
+  padding-right: 22px;
+  height: 100vh;
+  background-image: url(../../public/img/nopage/nopage4.jpg);
+  background-size:cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const Content = styled.div`
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+    font-size: 1rem;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: #f5f5f5;
+    border: none;
+    outline: 1px black solid;
+    color: #000;
+    cursor: pointer;
+    font-weight: bolder;
+    transition: background-color 0.3s ease, color 0.3s ease;
+
+    &:hover {
+      background-color: #131415;
+      color: white;
+    }
+`;
